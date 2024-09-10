95251b9cf28ce0fa404f5735fd6c07bf6db7965b
Alena Shpakova <53817236+alena-shp@users.noreply.github.com>
2024-09-10 17:29:36 +0400

    hook for form control

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,53 @@
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { logIn, signIn } from '@/shared/utils'
+import { type TFormType, EFormType } from '@/shared/types'
+import { type LoginSchema, type RegisterSchema, loginSchema, registerSchema } from '@/shared/schema'
+
+type Props = {
+	readonly formType: TFormType
+}
+
+export const useFormControl = ({ formType }: Props) => {
+	const navigate = useNavigate()
+
+	const schema = formType === EFormType.logIn ? loginSchema : registerSchema
+
+	const {
+		register,
+		handleSubmit,
+		reset,
+		setFocus,
+		formState: { isValid, errors }
+	} = useForm<LoginSchema & RegisterSchema>({ resolver: zodResolver(schema), mode: 'onTouched' })
+
+	useEffect(() => {
+		setFocus('username')
+	}, [setFocus])
+
+	const onSubmit = (data: LoginSchema & RegisterSchema) => {
+		const { username, password, email } = data
+
+		const userData = { name: username, password, email }
+
+		if (formType === EFormType.logIn) {
+			logIn(username)
+		} else {
+			signIn(userData)
+		}
+
+		reset()
+		navigate('/')
+	}
+
+	return {
+		register,
+		handleSubmit: handleSubmit(onSubmit),
+		isValid,
+		errors
+	}
+}
