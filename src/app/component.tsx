c4d161ddb5d57195a7cd3b543fe790a30c741e3d
Alena Shpakova <53817236+alena-shp@users.noreply.github.com>
2024-09-10 17:31:08 +0400

    code style

--- src/app/component.tsx
+++ src/app/component.tsx
@@ -2,19 +2,20 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Home } from '@/pages/home'
 import { Login } from '@/pages/login'
 import { Register } from '@/pages/register'
+import { PrivateRoute } from '@/features/privateRoute'
 import { Layout } from '@/entities/layout'
 
 const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      { path: '/', element: <Home /> },
-      { path: '/login', element: <Login /> },
-      { path: '/register', element: <Register /> },
-    ],
-  },
+	{
+		element: <Layout />,
+		children: [
+			{ path: '/', element: <PrivateRoute Component={Home} /> },
+			{ path: '/login', element: <Login /> },
+			{ path: '/register', element: <Register /> }
+		]
+	}
 ])
 
 export const App = () => {
-  return <RouterProvider router={router} />
+	return <RouterProvider router={router} />
 }
