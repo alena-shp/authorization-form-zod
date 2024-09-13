eb039bc7bc16c5e5495ab4d172d388dbb270f354
Alena Shpakova <53817236+alena-shp@users.noreply.github.com>
2024-09-13 16:56:33 +0400

    style home page

--- src/pages/home/component.tsx
+++ src/pages/home/component.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router'
 
 import { logOut, removeAccount } from '@/shared/utils'
 
+import $ from './styles.module.scss'
+
 export const Home = () => {
 	const navigate = useNavigate()
 
@@ -16,8 +18,13 @@ export const Home = () => {
 	}
 
 	return (
-		<div>
+		<div className={$.home}>
 			Home
+			<img
+				src="https://picsum.photos/400"
+				className={$.img}
+				alt="image"
+			></img>
 			<button onClick={handleLogOut}>Log out</button>
 			<button onClick={handleRemoveAccount}>Remove account</button>
 		</div>
